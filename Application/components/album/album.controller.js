6fc44a11ba2183a60da91657734e9a1fcc3da89e
Mil <mil@cogransystems.com>
2019-02-22 17:18:54 -0600

    fix 3 column display in album grid display

--- Application/components/album/album.controller.js
+++ Application/components/album/album.controller.js
@@ -1,7 +1,7 @@
 /**
 * @description Form name album - Album Maintenance
 * @version 1.0
-* @since Fri Feb 22 2019 16:08:30 GMT-0600 (Central Standard Time)
+* @since Fri Feb 22 2019 17:14:59 GMT-0600 (Central Standard Time)
 * @author Jin
 * @copyright Cogran Systems LLC
 */
